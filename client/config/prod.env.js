ad553f6529a5fd5727c8524acf736f86bcbe1fed
alikurtulush92 <kopanarya92@gmail.com>
2020-11-18 22:22:34 +0000

    make it better

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,4 +1,5 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  ROOT_API: '"https://pww-backend.herokuapp.com/api"'
 }
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -6,6 +6,7 @@ import ProductDetails from '@/components/Product/ProductDetails'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
